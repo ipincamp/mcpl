9146738ca549ebfbace25f43908f42d0ef610bea
ipincamp <arif.lngsrn@gmail.com>
2022-01-10 00:16:34 +0700

    add event listener & messages

--- FirstPlugin/src/me/ipincamp/FirstPlugin.java
+++ FirstPlugin/src/me/ipincamp/FirstPlugin.java
@@ -1,11 +1,14 @@
 package me.ipincamp;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirstPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("Plugin Enabled...");
+
+        Bukkit.getPluginManager().registerEvents(new HelloWorld(), this);
     }
 
     @Override
---
+++ FirstPlugin/src/me/ipincamp/HelloWorld.java
@@ -0,0 +1,18 @@
+package me.ipincamp;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class HelloWorld implements Listener {
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Material type = event.getBlock().getType();
+        if (type == Material.TORCH) {
+            Player player = event.getPlayer();
+            player.sendMessage("Hello world");
+        }
+    }
+}
