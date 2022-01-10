fb9d14b3d3627b812aad5532588b0fd3f90ae87f
ipincamp <arif.lngsrn@gmail.com>
2022-01-10 23:58:23 +0700

    Importing commands

--- FirstPlugin/src/me/ipincamp/FirstPlugin.java
+++ FirstPlugin/src/me/ipincamp/FirstPlugin.java
@@ -1,18 +1,30 @@
 package me.ipincamp;
 
+import me.ipincamp.commands.Feed;
+import me.ipincamp.commands.Heal;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@SuppressWarnings("ConstantConditions")
 public class FirstPlugin extends JavaPlugin {
+    private static FirstPlugin instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         System.out.println("Plugin Enabled...");
 
         Bukkit.getPluginManager().registerEvents(new HelloWorld(), this);
+        getCommand("heal").setExecutor(new Heal());
+        new Feed();
     }
 
     @Override
     public void onDisable() {
         System.out.println("Plugin Disabled...");
+    }
+
+    public static FirstPlugin getInstance() {
+        return instance;
     }
 }
